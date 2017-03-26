f74ba25cd3b3031810af05f12659890e1433639f
AhmadIsAwesome <pirza007.318@lgflmail.net>
2017-03-26 12:38:43 +0100

    Add files via upload

---
+++ ahmad_pirzad_10bb_cp_20170324_0.py
@@ -0,0 +1,106 @@
+import time
+def function():
+    Player = input("What is your name : ")
+    print("Welcome" ,Player)
+    print("Time to begin the quiz")
+    print("[Question 1]")
+    print("What is Data Consumption ? ")
+    time.sleep(1)
+    print("[A] The Consumption of Data")
+    time.sleep(1)
+    print("[B] The Consumption of energy or power")
+    time.sleep(1)
+    print("[C] When Data consumes consumption")
+    time.sleep(1)
+    answer = input(" A | B | C : ")
+    if answer == 'B':
+        print("Correct!")
+    elif answer =='A':
+        print("Wrong!")
+    elif answer =='C':
+        print("Wrong!")
+    else:
+        print("That's not an option")
+    print()
+    time.sleep(1)
+    print("[Question 2]")
+    print("what is what is E-Waste?")
+    time.sleep(1)
+    print("[A] Wasting energy")
+    time.sleep(1)
+    print("[B] Discarded electrical appliances")
+    time.sleep(1)
+    print("[C] Wasted Energy")
+    time.sleep(1)
+    answer2 = input(" A | B | C : ")
+    if answer2 == 'A':
+        print("Wrong")
+    elif answer2 =='B':
+        print("Correct")
+    elif answer2 =='C':
+        print("Wrong")
+    else:
+        print("That's not an option")
+    print()
+    time.sleep(1)
+    print("[Question 3]")
+    print("what is what TechoTrash?")
+    time.sleep(1)
+    print("[A] Wasted computer components")
+    time.sleep(1)
+    print("[B] tech thats trash")
+    time.sleep(1)
+    print("[C] trash that is energy")
+    time.sleep(1)
+    answer3 = input(" A | B | C : ")
+    if answer3 == 'A':
+        print("Correct")
+    elif answer3 =='B':
+        print("Wrong")
+    elif answer3 =='C':
+        print("Wrong")
+    else:
+        print("That's not an option")
+    print()
+    print("[Question 4]")
+    print("What act was made in 1998?")
+    time.sleep(1)
+    print("[A] The Data Protection Act")
+    time.sleep(1)
+    print("[B] The Copyright Infringment Act")
+    time.sleep(1)
+    print("[C] The Peaceful PC Act of 1998")
+    time.sleep(1)
+    answer4 = input(" A | B | C : ")
+    if answer4 == 'A':
+        print("Correct")
+    elif answer4 =='B':
+        print("Wrong")
+    elif answer4 =='C':
+        print("Wrong")
+    else:
+        print("That's not an option")
+    print()
+    print("[Question 5]")
+    print("What is Copyright ?")
+    time.sleep(1)
+    print("[A] the exclusive and assignable legal right,given to the originator for a fixed number of years to use")
+    time.sleep(1)
+    print("[B] the legal right that protects the use of your work once your idea has been physically expressed. ")
+    time.sleep(1)
+    print("[C] All of the Above")
+    time.sleep(1)
+    answer5 = input(" A | B | C : ")
+    if answer5 == 'A':
+        print("Wrong")
+    elif answer5 =='B':
+        print("Wrong")
+    elif answer5 =='C':
+        print("Correct")
+    else:
+        print("That's not an option")
+
+
+
+
+function()
